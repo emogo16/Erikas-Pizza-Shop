08e456c62523aaa2be4dc3feb888dcc191e228e2
Erika Mogollon <fany200025@hotmail.com>
2022-02-27 07:51:44 -0800

    it will modify UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -19,20 +19,30 @@ Pizza.prototype.calPrice=function() {
   
   if (this.base === "garlicParmesan" || this.base === "robustTomato") {
     this.price += 0;
-  
-  
   }
-
+  
+  if (this.cheese === "parmesan" || this.cheese === "cheese") {
+    this.price +=0
+  }
+  if (this.toppings === "mushrooms" || this.toppings === "spinach" || this.toppings === "pineapple" || this.toppings === "onion" || this.toppings === "pepper" || this.toppings === "pepperoni" ) {
+    this.price +=0
+  } else if 
+    (this.toppings === "chicken" || this.toppings === "bacon" || this.toppings === "chicken" || this.toppings === "sausage") {
+      this.price += 3
+      return this.price
   }
 }
+
 
   
 // UI logic
 $(document).ready(function() {
   $("button").submit(function(event) {
-    let size = $("#pizza-size").find(":selected").val();
-    let base = $("#pizza-size").find(":selected").val();
-    let cheese = $("#pizza-size").find(":selected").val();
+    let size = $("input[name='optcheckbox size'):checked").val();
+    let base = $("input[name='optcheckbox base'):checked").val();
+    let cheese = $("input[name='optcheckbox cheese'):checked").val();
+    let toppings = $("input[name='optcheckbox toppings') :checked").val();
+    let newPizza =
     event.preventDefault();
     const
   
