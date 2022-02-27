92921eadbccb1dfbfd53d727af5f6b73282dedf1
Erika Mogollon <fany200025@hotmail.com>
2022-02-26 23:08:48 -0800

    WIP

---
+++ README.md
@@ -0,0 +1,5 @@
+Describe: Pizza()
+
+Test: "It should return a pizza object with the property for size and price
+Code: constmyPizza=("small")
+Expected Output= Pizza {size:"small, medium, or large"}
--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,35 @@ function Pizza(size, base, cheese, toppings, price ) {
   this.cheese=cheese
   this.toppings=toppings
   this.price=0
-  }
+}
+
+Pizza.prototype.calPrice=function() {
+  if (this.size === "small") {
+    this.price += 9;
+  } else if
+    (this.size === "medium") {
+      this.price += 10;
+  } else if
+    (this.size === "large") {
+      this.price += 11;
   
-//UI logic
-$(document).ready(function() {
-  $(#formOne).submit(function(event) {
-    event.preventDefault();
-    const
+  if (this.base === "garlicParmesan" || this.base === "robustTomato") {
+    this.price += 0;
+  
+  
+  }
+
   }
 }
+
+  
+// UI logic
+$(document).ready(function() {
+  $("button").submit(function(event) {
+    let size = $("#pizza-size").find(":selected").val();
+    let base = $("#pizza-size").find(":selected").val();
+    let cheese = $("#pizza-size").find(":selected").val();
+    event.preventDefault();
+    const
+  
+});
