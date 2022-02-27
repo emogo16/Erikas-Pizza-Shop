45442596b846c0c1b56ba25a8f5856dbc8a0a757
Erika Mogollon <fany200025@hotmail.com>
2022-02-26 22:32:33 -0800

    it will add more in scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,16 @@
 //business logic
-function pizza()
-  let 
-
-
-
-
-
-
+function Pizza(size, base, cheese, toppings, price ) {
+  this.size=size
+  this.base=base
+  this.cheese=cheese
+  this.toppings=toppings
+  this.price=0
+  }
+  
 //UI logic
 $(document).ready(function() {
-
+  $(#formOne).submit(function(event) {
+    event.preventDefault();
+    const
+  }
 }
